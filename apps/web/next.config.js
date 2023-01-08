043f0512ab8810ebd1bf777a2c695c246f5f6e41
Roman Zhuravlov <snelsithepirate@gmail.com>
2023-01-08 14:47:54 +0200

    Update project setup

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,8 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    transpilePackages: ["ui"],
-  },
+  transpilePackages: ["ui"],
 };
+
+module.exports = nextConfig;
